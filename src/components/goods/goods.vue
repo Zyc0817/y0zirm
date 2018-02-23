4cfc476ef2faecf9db75f174125eeefe7082b83a
Zyc0817 <z815517777@163.com>
2018-02-23 12:54:28 +0800

    header

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
+
 </template>
 
 <script type="text/ecmascript-6">
   export default {};
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+
+  .goods
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    display: flex
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
+
 
 </style>
